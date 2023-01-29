c96c70085847a6590983cbdefb0a415de7424797
Natalya.Kadysheva <Native1331>
2023-01-29 16:01:17 +0300

    Add Lesson33

---
+++ src/main/java/Lesson33.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Lesson33 {
+    public static void main(String[] args) {
+        /////////////////Java 5///////////
+        List animals = new ArrayList();
+        animals.add("cat");
+        animals.add("dog");//1
+        animals.add("frog");
+        animals.add(ourAnimal);
+
+        String animal =(String) animals.get(1);
+        System.out.println(animal);
+
+        ////////////С появлением Generics///////////
+        List<String> animals2=new ArrayList<String>();
+        animals2.add("cat");
+        animals2.add("dog");//1
+        animals2.add("frog");
+        String animal2=animals2.get(1);
+
+        ///////////////С появлением Java7///////////
+        List<String> animals3=new ArrayList<>();
+        animals3.add("cat");
+        animals3.add("dog");//1
+        animals3.add("frog");
+        //animals3.add(ourAnimal);
+        String animal2=animals2.get(1);
+
+
+
+    }
+}
