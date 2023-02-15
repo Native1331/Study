d8fd4114795be96b12be9d47fcfbdb80002a0d52
Natalya.Kadysheva <Native1331>
2023-02-15 10:59:13 +0300

    Add Lesson 43

---
+++ src/main/java/Lesson43/Electrocar.java
@@ -0,0 +1,41 @@
+package Lesson43;
+    public  class Electrocar {
+        private int id;
+//вложенный нестатический класс
+        private class Motor{
+            public void starMotor() {
+                System.out.println("Motor"+id+ "is starting...");
+            }
+        }
+        //вложенный статический класс
+        public static class Battery {
+            public void charge() {
+                System.out.println("Battery is charging...");
+            }
+
+        }
+
+        public Electrocar(int id){
+            this.id = id;
+        }
+        public void start() {
+            Motor motor = new Motor();
+            motor.starMotor();
+
+            final int x = 1;
+
+            class SomeClass{
+                public void someMethod() {
+                    System.out.println(x);
+                    System.out.println(id);
+
+                }
+            }
+            SomeClass someObject= new SomeClass();
+            System.out.println("Electrocar"+id+" is starting...");
+        }
+        private void test(){
+
+        }
+    }
+
